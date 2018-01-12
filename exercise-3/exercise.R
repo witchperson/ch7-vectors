408a2b77082017ae767895a64455cadcc7135791
Joel Ross <joelross@uw.edu>
2018-01-12 15:02:59 -0800

    Add exercises

---
+++ exercise-3/exercise.R
@@ -0,0 +1,31 @@
+# Exercise 5: vector practice
+
+# Create a vector `words` of 6 (or more) words.
+# You can Google for a "random word generator" if you wish!
+
+
+# Create a vector `words.of.the.day` that is your `words` vector with the string
+# "is the word of the day!" pasted on to the end.
+# BONUS: Surround the word in quotes (single or double) (e.g., `"data" is the 
+# word of the day!`)
+
+
+# Create a vector `a.f.words` which are the elements in `words` that start with 
+# "a" through "f"
+# Hint: use a comparison operator to see if the word comes before "f" alphabetically!
+# Tip: make sure all the words are lower-case, and only consider the first letter
+# of the word!
+
+
+# Create a vector `g.m.words` which are the elements in `words` that start with 
+# "g" through "m"
+
+
+# Define a function `WordBin` that takes in three arguments: a vector of words, 
+# and two letters. The function should return a vector of words that go between 
+# those letters alphabetically.
+
+
+# Use your `WordBin` function to determine which of your words start with "e" 
+# through "q"
+
