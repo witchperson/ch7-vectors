fe61c788bc785d835aecf68722249d93d0ebb87d
witchperson <kangwooc@gmail.com>
2018-01-18 10:54:40 -0800

    finish exercise

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,47 +2,47 @@
 
 # Create a vector `first.ten` that has the values 10 through 20 in it (using 
 # the : operator)
-
+first.ten <- 10:20
 
 # Create a vector `next.ten` that has the values 21 through 30 in it (using the 
 # seq() function)
-
+next.ten <- seq(21, 30)
 
 # Create a vector `all.numbers` by combining the previous two vectors
-
+all.numbers <- first.ten + next.ten
 
 # Create a variable `eleventh` that contains the 11th element in `all.numbers`
-
+eleventh <- all.numbers[11]
 
 # Create a vector `some.numbers` that contains the 2nd through the 5th elements 
 # of `all.numbers`
-
+some.numbers <- all.numbers[c(2,5)]
 
 
 # Create a vector `even` that holds the even numbers from 1 to 100
-
+even <- numbers[c(1:100) %% 2==0] 
 
 # Using the `all()` function and `%%` (modulo) operator, confirm that all of the
 # numbers in your `even` vector are even
-
+all(even %% 2 ==0)
 
 
 # Create a vector `phone.numbers` that contains the numbers 8,6,7,5,3,0,9
-
+phone.numbers <- c(8, 6, 7, 5, 3, 0, 9)
 
 # Create a vector `prefix` that has the first three elements of `phone.numbers`
-
+prefix <- phone.numbers[1:3]
 
 # Create a vector `small` that has the values of `phone.numbers` that are 
 # less than or equal to 5
-
+small <- phone.numbers[phone.numbers<=5]
 
 # Create a vector `large` that has the values of `phone.numbers` that are 
 # greater than 5
-
+large <- phone.numbers[phone.numbers>5]
 
 # Replace the values in `phone.numbers` that are larger than 5 with the number 5
-
+phone.numbers[phone.numbers>5] <- 5
 
 # Replace every odd-numbered value in `phone.numbers` with the number 0
-
+phone.numbers[phone.numbers%%2==1] <- 0
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -2,30 +2,32 @@
 
 # Create a vector `words` of 6 (or more) words.
 # You can Google for a "random word generator" if you wish!
-
+words <- c("establish", "heir", "harass", "claim", "manufacturer","activate")
 
 # Create a vector `words.of.the.day` that is your `words` vector with the string
 # "is the word of the day!" pasted on to the end.
 # BONUS: Surround the word in quotes (single or double) (e.g., `"data" is the 
 # word of the day!`)
-
+words.of.the.day <- paste(paste0("\'",words,"\'"), "is the word of the day!")
 
 # Create a vector `a.f.words` which are the elements in `words` that start with 
 # "a" through "f"
 # Hint: use a comparison operator to see if the word comes before "f" alphabetically!
 # Tip: make sure all the words are lower-case, and only consider the first letter
 # of the word!
-
+a.f.words <- words[words <= "f"]
 
 # Create a vector `g.m.words` which are the elements in `words` that start with 
 # "g" through "m"
-
+g.m.word <- words[words >= "g" & words <= "m"]
 
 # Define a function `WordBin` that takes in three arguments: a vector of words, 
 # and two letters. The function should return a vector of words that go between 
 # those letters alphabetically.
-
+WordBin <- function (words, letter.first, letter.second) {
+  return (sort(words[words >=letter.first & words <= letter.second]))
+}
 
 # Use your `WordBin` function to determine which of your words start with "e" 
 # through "q"
-
+WordBin(words, "e", "q")
